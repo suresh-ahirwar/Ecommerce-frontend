485c47383c6d57bdc438b677c413321beb440665
suresh ahirwar <ahirwarsuresh077@gmail.com>
2025-02-05 13:26:17 +0530

    Homepage completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,37 @@
+import { Categories } from "../assets/mockData";
+import heroimg from "../assets/images/heroimg.jpg";
 function Home() {
     return (
-        <div>
-           Home 
+        <div className="bg-white mt-2 px-4 md:px-16 lg:px-24">
+            <div className="container mx-auto py-4 flex flex-col md:flex-row space-x-2">
+                <div className="w-full md:w-3/12">
+                    <div className="bg-red-600 text-white text-xs font-bold px-2 py-2.5">
+                        SHOP BY CATAGORIES
+                    </div>
+                    <ul className="space-y-4 bg-gray-100 p-3 border border-gray-100">
+                        {Categories.map((category, index) => {
+                            return (
+                                <li key={index} className="flex items-center text-sm font-medium">
+                                    <div className="w-2 h-2 border border-red-500 rounded-full mr-2"></div>
+                                    {category}
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+
+                <div className="w-full md:w-9/12 mt-8 md:mt-0 h-96 relative">
+                    <img src={heroimg} alt="" className="w-full h-full" />
+                    <div className="absolute top-16 left-8">
+                        <p className="text-gray-600 mb-4">Code With Suresh</p>
+                        <h2 className="text-3xl font-bold">WELCOME TO E-SHOP </h2>
+                        <p className="text-xl mt-2.5 font-bold text-gray-800">MILLIONS+ PROUDUCTS</p>
+                        <button className="bg-red-600 px-8 py-1.5 text-white mt-4 hover:bg-red-700 transform transition-transform duration-300 hover:scale-105">SHOP NOW</button>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
