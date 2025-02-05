3db2bb3442bd06ad018edc098b9d7ddc7bbffc23
suresh ahirwar <ahirwarsuresh077@gmail.com>
2025-02-05 11:23:08 +0530

    Navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,25 +9,43 @@ function Navbar() {
                    <Link to="/">e-SHOP</Link>
                 </div>
                 
-                <div className="font-bold text-lg mx-4">
+                <div className="font-bold flex-1 mx-4">
                    <form className="relative flex-1">
-                        <input type="text" placeholder="Search Product" className="w-full border py-2 px-4"/>
+                        <input type="text" placeholder="Search Product" className="w-full  border border-gray-300 py-2 px-4"/>
                         <FaSearch className="absolute top-3 right-3 text-red-500"></FaSearch>
                    </form>
 
                 </div>
                
-                <div>
+                <div className="flex items-center space-x-4">
                  <Link to="/cart">
-                 <FaShoppingCart />
+                 <FaShoppingCart className="text-lg"/>
                  </Link>
-                 <button>
+                 <button className="hidden md:block">
                     Login/Register
                  </button>
-                 <button>
+                 <button className="block md:hidden">
                     <FaUser />
                  </button>
                 </div>
+            </div>
+
+            <div className="flex justify-center items-center space-x-10 y-4 text-sm font-bold pb-2">
+                <Link to="/"className="hover:underline">
+                Home
+                </Link>
+
+                <Link to="/"className="hover:underline">
+                Shop
+                </Link>
+
+                <Link to="/"className="hover:underline">
+                Contact 
+                </Link>
+
+                <Link to="/"className="hover:underline">
+                About
+                </Link>
             </div>
         </nav>
     )
