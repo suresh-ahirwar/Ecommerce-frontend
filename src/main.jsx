ea4e60021933e97d9dedd222fcfb5fa718b533f9
suresh ahirwar <ahirwarsuresh077@gmail.com>
2025-02-06 12:17:47 +0530

    Revert "Productcard is completed"
    This reverts commit ff9fed32e3824ef3a248c2bd3cbd2585e5531352.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { Provider } from "react-redux";
-import store from "./redux/store.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
