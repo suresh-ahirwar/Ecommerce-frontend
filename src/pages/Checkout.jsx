e95ff3b6b6d2eec54c3cfdfc24ad4a111d883905
suresh ahirwar <ahirwarsuresh077@gmail.com>
2025-02-14 22:05:58 +0530

    order summary card is done

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 const Checkout = () => {
+  const cart = useSelector(state => state.cart)
   const [billingToggle,setBillingToggle] = useState(true);
   const [shippingToggle,setShippingToggle] = useState(false);
   const [paymentToggle,setPaymentToggle] = useState(false);
   const [paymentMethod,setPaymentMethod] = useState("cod");
+  
   return (
     <div className="container mx-auto py-8 min-h-96 px-4 md:px-16 lg:px-24  border-1">
       <h3 className="text-2xl font-semibold mb-4">CHECKOUT</h3>
@@ -122,7 +125,31 @@ const Checkout = () => {
 
           </div>
         </div>
-        <div className="md:w-1/3 bg-white p-6 rounded-lg shadow-md border border-gray-300"></div>
+        <div className="md:w-1/3 bg-white p-6 rounded-lg shadow-md border border-gray-300">
+        <h3 className="text-lg font-semibold mb-4">Order Summary</h3>
+        <div className="space-y-4">
+          {cart.products.map((product)=>(
+            <div key={product.id} className="flex justify-between"> 
+              <div className="flex items-center">
+                <img src={product.image} alt={product.name}  className="w-16 h-16 object-contain rounded"/>
+                <div className="ml-4">
+                  <h4 className="text-md font-semibold">{product.name}</h4>
+                  <p className="text-gray-600">
+                    ${product.price} * {product.quantity}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="mt-4 border-t border-gray-400 pt-4">
+          <div className="flex justify-between">
+            <span>Total Price:</span>
+            <span className="font-semibold">${cart.totalPrice.toFixed(2)}</span>
+          </div>
+        </div>
+        <button className="w-full bg-red-600 text-white py-2 mt-6 hover:bg-red-800">Place Order</button>
+        </div>
       </div>
     </div>
   )
